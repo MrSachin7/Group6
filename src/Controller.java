493ae429d82081ddaf1f2ef2c79695ec58aab8f9
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-05-21 01:00:03 +0200

    Controller is being ready

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,41 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller
 {
-public void changeShirtNumber()
-{
+  @FXML private Button updatePlayers;
+  @FXML private Button savePlayer;
+  @FXML private Button savePosition;
+  @FXML private Button createPlayer;
+  @FXML private Button createMatch;
+  @FXML private Button upcomingMatchesUpdate;
+  @FXML private Button previousMatchesUpdate;
+  @FXML private Button addInjury;
+  @FXML private Button removeInjury;
+  @FXML private Button addSuspension;
+  @FXML private Button removeSuspension;
+  @FXML private TextArea allPlayersName;
+  @FXML private TextArea allPlayersPosition;
+  @FXML private TextArea allPlayersShirtNumber;
+  @FXML private ChoiceBox choiceBoxShirtNo;
+  @FXML private Spinner spinnerShirtNo;
+  @FXML private ChoiceBox choiceBoxPosition;
+  @FXML private TextField textFieldPosition;
+  @FXML private TextField createPlayerFirstName;
+  @FXML private TextField createPlayerLastName;
+  @FXML private TextField createPlayerPosition;
+  @FXML private Spinner createPlayerShirtNumber;
+  @FXML private DatePicker createPlayerDate;
+  @FXML private TextArea upcomingMatchesTextArea;
+  @FXML private TextArea previousMatchesTextArea;
+  @FXML private DatePicker createMatchDate;
+  @FXML private TextField createMatchOpponent;
+  @FXML private RadioButton createMatchHome;
+  @FXML private RadioButton createMatchAway;
+  @FXML private RadioButton createMatchLeague;
+  @FXML private RadioButton createMatchCup;
+  @FXML private RadioButton createMatchFriendly;
+  @FXML private Spinner createMatchHour;
+  @FXML private Spinner createMatchMinute;
 
-}
-public void changePosition()
-{
-
-}
 }
